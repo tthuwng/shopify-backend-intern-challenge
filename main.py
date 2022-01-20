44b6615e8017b5c057bb2c5a2eba3f3599ed657e
tthuwng <tronghung199@gmail.com>
2022-01-20 00:39:59 -0600

    update image handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
-import sqlite3
+import sqlite3, os
 from werkzeug.utils import secure_filename
 
 
@@ -26,7 +26,18 @@ def get_inventory(inventory_id):
         abort(404)
     return inventory
 
+def isFloat(element):
+    try:
+        if float(element) > 0:
+            return True
+        return False
+    except ValueError:
+        return False
 
+
+@app.route('/upload/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/<int:id>/edit/', methods=('GET', 'POST'))
 def edit(id):
@@ -35,67 +46,83 @@ def edit(id):
     if request.method == 'POST':
         name = request.form['name']
         description = request.form['description']
-        price = float(request.form['price'])
-        stock = int(request.form['stock'])
+        price = request.form['price']
+        stock = request.form['stock']
 
-        #Uploading image procedure
         image = request.files['image']
-        if image and allowed_file(image.filename):
+        imagename = inventory[4]
+        if image.filename != '':
+          
+          if image and allowed_file(image.filename):
             filename = secure_filename(image.filename)
             image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        imagename = filename
-        print(imagename)
+          imagename = filename
+        
+        
 
         if not name:
             flash('Name is required!')
 
         elif not description:
             flash('description is required!')
-        
         elif not price:
             flash('Price is required!')
-
-        elif not image:
-            flash('Image is required!')
+        
+        elif not isFloat(price):
+            flash('Price must be positive!')
+        elif stock and not stock.isdigit():
+            flash('Stock must be positive!')
+        # elif not image:
+        #     flash('Image is required!')
 
         else:
-            conn = get_db_connection()
-            conn.execute('UPDATE inventories SET name = ?, description = ?, price = ?, image = ?, stock = ?'
-                         ' WHERE id = ?',
-                         (name, description, price, imagename, stock, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+          
+          
+
+          conn = get_db_connection()
+          conn.execute('UPDATE inventories SET name = ?, description = ?, price = ?, image = ?, stock = ?'
+                        ' WHERE id = ?',
+                        (name, description, float(price), imagename, int(stock), id))
+          conn.commit()
+          conn.close()
+          return redirect(url_for('index'))
     return render_template('edit.html', inventory=inventory)
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
   if request.method == 'POST':
-    print(request.form)
     name = request.form['name']
     description = request.form['description']
-    price = float(request.form['price'])
-    stock = int(request.form['stock'])
-
+    price = request.form['price']
+    stock = request.form['stock']
     image = request.files['image']
-    if image and allowed_file(image.filename):
-        filename = secure_filename(image.filename)
-        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    imagename = filename
-
-    if not name:
+    
+    if image.filename == '':
+      flash('No selected image')
+    elif not name:
       flash('Name is required!')
     elif not description:
       flash('Description is required!')
     elif not price:
       flash('Price is required!')
+    elif not isFloat(price):
+      flash('Price must be positive!')
+    elif stock and not stock.isdigit():
+      flash('Stock must be positive!')
     elif not image:
       flash('Image is required!')
 
 
     else:
+      if image and allowed_file(image.filename):
+        filename = secure_filename(image.filename)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      imagename = filename
+
       conn = get_db_connection()
-      conn.execute('INSERT INTO inventories (name, description, price, image, stock) VALUES (?, ?)', (name, description, price, imagename, stock))
+      price = float(price)
+      stock = int(stock)
+      conn.execute('INSERT INTO inventories (name, description, price, image, stock) VALUES (?, ?, ?, ?, ?)', (name, description, price, imagename, stock))
       conn.commit()
       conn.close()
       return redirect(url_for('index'))
@@ -123,7 +150,3 @@ def index():
   inventories = conn.execute('SELECT * FROM inventories').fetchall()
   conn.close()
   return render_template('index.html', inventories=inventories)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
